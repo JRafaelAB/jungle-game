d037c186ac6b8cb523511083ec90832c6aa2c634
Jonas Borges <jonasrafael97@hotmail.com>
2024-05-16 10:26:14 -0300

    Push context

---
+++ Infrastructure/DataAccess/Contexts/UsersContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Domain.Entities;
+using Domain.Utils;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DataAccess.Contexts;
+
+[ExcludeFromCodeCoverage]
+public class UsersContext : DbContext
+{
+
+    public virtual DbSet<User> Users { get; init; } = null!;
+    
+    public UsersContext()
+    {
+    }
+
+    public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ValidateNullArgument(nameof(modelBuilder));
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(UsersContext).Assembly);
+    }
+}
