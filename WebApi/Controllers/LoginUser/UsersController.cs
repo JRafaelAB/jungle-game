e9ad943f5f6d8f92f1d4900d8cddeaeb71734f7e
Jonas Borges <jonasrafael97@hotmail.com>
2024-05-24 11:35:50 -0300

    Add not found response type to loginUser controller

--- WebApi/Controllers/LoginUser/UsersController.cs
+++ WebApi/Controllers/LoginUser/UsersController.cs
@@ -16,10 +16,12 @@ public class UsersController(ILoginUserUseCase useCase) : BaseController
     /// </summary>
     /// <response code="200">Successfull request.</response>
     /// <response code="400">Invalid Request.</response>
+    /// <response code="404">User Not Found.</response>
     /// <param name="request"></param>
     [HttpPost("Login")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> LoginUser([FromBody] LoginUserRequest request)
     {
         ValidateRequest(request);
