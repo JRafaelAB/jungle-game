737fff506ca73a91ac4ef40f9a0e1a2b048431ed
Jonas Borges <jonasrafael97@hotmail.com>
2024-05-24 15:15:58 -0300

    Add new endpoint test

---
+++ Tests/UnitTests/Controllers/GetUser/UserControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Application.UseCases.GetUser;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WebApi.Controllers.GetUser;
+using Xunit;
+
+namespace UnitTests.Controllers.GetUser;
+
+public class UsersControllerTest
+{
+    private readonly UsersController _controller;
+    private readonly Mock<IGetUserUseCase> _loginUser = new ();
+
+    public UsersControllerTest()
+    {
+        this._controller = new UsersController(_loginUser.Object);
+    }
+
+    [Fact]
+    public async Task Test_Get_Existing_User()
+    {
+        var result = await this._controller.GetUser(1);
+        Assert.IsType<OkObjectResult>(result);
+    }
+    
+}
