98a7a1fbd9a46e1dc84f9de4b091217875e2ebf8
Jonas Borges <jonasrafael97@hotmail.com>
2024-05-24 12:42:56 -0300

    Add GetUserUseCaseTest

--- Domain/Models/Responses/GetUserResponse.cs
+++ Domain/Models/Responses/GetUserResponse.cs
@@ -6,4 +6,32 @@ public class GetUserResponse(UserDto user)
 {
     public ulong? Id { get; } = user.Id;
     public string Username { get; } = user.Username;
+    
+
+    protected bool Equals(GetUserResponse other)
+    {
+        return this.Username == other.Username && this.Id == other.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == this.GetType() && Equals((GetUserResponse)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.Username, this.Id);
+    }
+
+    public static bool operator ==(GetUserResponse? left, GetUserResponse? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(GetUserResponse? left, GetUserResponse? right)
+    {
+        return !Equals(left, right);
+    }
 }
---
+++ Tests/UnitTests/UseCases/GetUser/GetUserUseCaseTest.cs
@@ -0,0 +1,43 @@
+﻿using Application.UseCases.GetUser;
+using Domain.Exceptions;
+using Domain.Models.Responses;
+using Domain.Repositories;
+using Domain.Resources;
+using Moq;
+using Xunit;
+
+namespace UnitTests.UseCases.GetUser;
+
+public class GetUserUseCaseTest
+{
+    private readonly Mock<IUserRepository> _userRepository;
+    private readonly IGetUserUseCase _useCase;
+    
+    public GetUserUseCaseTest()
+    {
+        _userRepository = new Mock<IUserRepository>();
+        ConfigureUserRepositoryForExistingUser();
+        _useCase = new GetUserUseCase(_userRepository.Object);
+    }
+    
+    
+
+    [Fact]
+    public async Task Test_Get_Existing_User()
+    {
+        var result = await this._useCase.Execute(1);
+        Assert.Equal(new GetUserResponse(JungleContextMock.User1Dto), result);
+    }
+
+    [Fact]
+    public async Task Test_Get_Non_Existing_User()
+    {
+        var ex = await Assert.ThrowsAsync<UserNotFoundException>(() => this._useCase.Execute(10));
+        Assert.Equal(Messages.UserNotFound, ex.Message);
+    }
+    
+    private void ConfigureUserRepositoryForExistingUser()
+    {
+        this._userRepository.Setup(repo => repo.GetUser(1)).ReturnsAsync(JungleContextMock.User1Dto);
+    }
+}
