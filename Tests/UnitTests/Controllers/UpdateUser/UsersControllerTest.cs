2165cd9ebf7563ecbf332a8c76d2962d2e3bab38
Jonas Borges <jonasrafael97@hotmail.com>
2024-07-30 14:34:03 -0300

    Removed unnecessary usings

--- Tests/UnitTests/Controllers/UpdateUser/UsersControllerTest.cs
+++ Tests/UnitTests/Controllers/UpdateUser/UsersControllerTest.cs
@@ -2,7 +2,6 @@
 using Domain.Exceptions;
 using Domain.Models.Requests;
 using Domain.Resources;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Newtonsoft.Json;
